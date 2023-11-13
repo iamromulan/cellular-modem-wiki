dc7c5228ca8fb0d24cdfd7bf0c51ecba7c57c4d3
iamromulan <50184035+iamromulan@users.noreply.github.com>
2023-11-12 21:59:17 -0500

    Update atcommand.sh
    update to sh format

--- files/atcommand.sh
+++ files/atcommand.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 DEVICE_FILE="/dev/smd7"
 TIMEOUT=4
 
-# Check if the device file exists
+# Check if the device file exists and is a character special file
 if [ ! -c "$DEVICE_FILE" ]; then
     echo "Error: Device $DEVICE_FILE does not exist or is not a character special file."
     exit 1
@@ -12,47 +12,31 @@ fi
 # Ask for the AT command from the user
 read -p "Enter AT command: " at_command
 
-# Function to start cat in the background, send the AT command, and output the response
-function send_at_command {
-    # Start 'cat' in the background and redirect output to a temporary file
-    cat "$DEVICE_FILE" > /tmp/device_output &
+# Function to send the AT command to the device and read the response
+send_at_command() {
+    # Flush any existing content in the device buffer
+    cat $DEVICE_FILE > /dev/null 2>&1 & 
     CAT_PID=$!
-
-    # Wait a little bit to ensure 'cat' is ready
-    sleep 0.5
+    sleep 0.1
+    kill $CAT_PID
+    wait $CAT_PID 2>/dev/null
 
     # Send the AT command to the device
     echo -e "${at_command}\r" > "$DEVICE_FILE"
 
-    # Show a waiting message
-    echo "Waiting for response..."
-
-    # Wait for response with a timeout
-    {
-        sleep "$TIMEOUT" && kill $CAT_PID
-    } &
-
-    WAIT_PID=$!
-
-    # Wait for either cat to finish or the timeout
-    wait $CAT_PID 2>/dev/null
-    EXIT_STATUS=$?
-
-    # Kill the sleep command if it's still running
-    kill $WAIT_PID 2>/dev/null
-
-    # Check if 'cat' was successful
-    if [ $EXIT_STATUS -eq 0 ]; then
-        # 'cat' exited normally, show the output
-        cat /tmp/device_output
-    else
-        # 'cat' failed or was killed, assume timeout
-        echo "Error: Response timed out."
-    fi
-
-    # Clean up
-    rm /tmp/device_output
+    # Read the response for a certain time period
+    (cat "$DEVICE_FILE" & sleep "$TIMEOUT"; kill $!) | {
+        # Read from the pipe until there is no more data or timeout occurs
+        while IFS= read -r line; do
+            RESPONSE="${RESPONSE}${line}\n"
+        done
+        # After the subshell and background jobs are done, check if we got a response
+        if [ -z "$RESPONSE" ]; then
+            echo "Error: Response timed out."
+        else
+            echo "$RESPONSE"
+        fi
+    }
 }
 
-# Run the function
 send_at_command
