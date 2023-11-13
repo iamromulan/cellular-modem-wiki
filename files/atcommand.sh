11cab3686da13b9bc2338203f24bcf64d5e3d4ca
iamromulan <50184035+iamromulan@users.noreply.github.com>
2023-11-12 21:29:51 -0500

    Update atcommand.sh
    check for commands first

--- files/atcommand.sh
+++ files/atcommand.sh
@@ -1,40 +1,50 @@
 #!/bin/bash
 
-# Path to the device file
 DEVICE_FILE="/dev/smd7"
 
 # Check if the device file exists
-if [ ! -e "$DEVICE_FILE" ]; then
-    echo "Error: Device file $DEVICE_FILE does not exist."
+if [ ! -c "$DEVICE_FILE" ]; then
+    echo "Error: Device $DEVICE_FILE does not exist or is not a character special file."
     exit 1
 fi
 
-# Function to read the AT command from user input
-read_at_command() {
-    read -p "Enter AT command: " at_command
-    echo -e "$at_command\r"
-}
+# Ask for the AT command from the user
+read -p "Enter AT command: " at_command
 
-# Function to send the AT command to the device and read the response
-send_and_read_response() {
-    # Send the AT command to the device file
-    read_at_command > "$DEVICE_FILE"
+# Function to display a waiting message and time out after 4 seconds
+wait_with_message() {
+    local timeout=$1
+    local waiting_time=0
+    local wait_interval=1
 
-    # Display waiting message
     echo "Waiting for response..."
+    while [ $waiting_time -lt $timeout ]; do
+        if read -r line <&3; then
+            echo "$line"
+            return 0
+        fi
+        sleep $wait_interval
+        waiting_time=$((waiting_time + wait_interval))
+    done
 
-    # Use 'timeout' to run 'cat' with a 4-second limit
-    response=$(timeout 4 cat "$DEVICE_FILE")
-
-    # Check if timeout command timed out
-    if [ $? -eq 124 ]; then
-        echo "Error: Response timed out."
-    else
-        # Display the response
-        echo "Response from device:"
-        echo "$response"
-    fi
+    echo "Error: Response timed out after ${timeout} seconds."
+    return 1
 }
 
-# Execute the send and read function
-send_and_read_response
+# Send the AT command and capture the response
+capture_response() {
+    # Start 'cat' in the background on the device file, redirected to file descriptor 3
+    exec 3< <(cat "$DEVICE_FILE")
+
+    # Send the AT command to the device
+    echo -e "$at_command\r" > "$DEVICE_FILE"
+
+    # Call the wait_with_message function with a 4-second timeout
+    wait_with_message 4
+
+    # Clean up background process
+    exec 3<&-
+}
+
+# Run the capture_response function
+capture_response
