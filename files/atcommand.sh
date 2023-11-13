97bc032a702240b66406bd475a596cd9224342bf
iamromulan <50184035+iamromulan@users.noreply.github.com>
2023-11-12 23:22:46 -0500

    Update atcommand.sh

--- files/atcommand.sh
+++ files/atcommand.sh
@@ -3,46 +3,55 @@
 DEVICE_FILE="/dev/smd7"
 TIMEOUT=4
 
-# Function to send AT command and read response
-send_at_command() {
-    # Clear any previous output in the device file
-    cat "$DEVICE_FILE" > /dev/null 2>&1 & 
+# Function to prepare reading from the device
+prepare_device_read() {
+    # Clear any existing data and start a new background read process
+    cat "$DEVICE_FILE" > /dev/null 2>&1 &
     CAT_PID=$!
-    sleep 0.2
+    sleep 0.1
     kill -9 $CAT_PID
     wait $CAT_PID 2>/dev/null
+    cat "$DEVICE_FILE" > /tmp/device_output &
+    CAT_PID=$!
+    echo "Background read on $DEVICE_FILE started with PID $CAT_PID"
+}
 
-    # Send the AT command
-    printf "$1\r" > "$DEVICE_FILE"
-    sleep 0.5  # Wait half a second to ensure the command is sent
+# Function to send AT command to the device
+send_at_command() {
+    # Ask for the AT command from the user
+    printf "Enter AT command: "
+    read at_command
+    # Send the AT command to the device
+    echo -e "${at_command}\r" > "$DEVICE_FILE"
+    echo "Command sent to the device. Waiting for response..."
+}
 
-    # Start reading the response
-    RESPONSE=""
-    exec 3<> "$DEVICE_FILE" # Open the device file for reading and writing
-    (sleep "$TIMEOUT"; echo) >&3 & # Send a delayed newline to unblock 'read'
-    while IFS= read -r line <&3; do
-        RESPONSE="${RESPONSE}${line}\n"
-        if [ -n "$line" ] && [ "$line" = "OK" ]; then
-            break
-        fi
-    done
-
-    # Output the response
+# Function to display the device's response
+display_response() {
+    # Allow some time for the response to be received
+    sleep $TIMEOUT
+    # Kill the background read process
+    kill -9 $CAT_PID 2>/dev/null
+    wait $CAT_PID 2>/dev/null
+    # Display the response
+    RESPONSE=$(< /tmp/device_output)
     if [ -z "$RESPONSE" ]; then
-        echo "Error: No response received, or response timed out."
+        echo "Error: No response received or response timed out."
     else
         echo "Response from device:"
         echo "$RESPONSE"
     fi
-    exec 3<&- # Close the file descriptor
+    # Clean up the temporary file
+    rm -f /tmp/device_output
 }
 
-# Main logic
+# Check if the device file exists and is accessible
 if [ ! -c "$DEVICE_FILE" ]; then
-    echo "Error: Device $DEVICE_FILE does not exist or is not a character special file."
+    echo "Error: The device $DEVICE_FILE does not exist or is not a character special file."
     exit 1
 fi
 
-echo "Enter AT command:"
-read at_command
-send_at_command "$at_command"
+# Run the command reading sequence
+prepare_device_read
+send_at_command
+display_response
