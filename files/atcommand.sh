387d3b4d55bbae064991fdf1a6b4161dc4c6ca44
iamromulan <50184035+iamromulan@users.noreply.github.com>
2023-11-12 23:49:39 -0500

    Update atcommand.sh

--- files/atcommand.sh
+++ files/atcommand.sh
@@ -1,49 +1,48 @@
 #!/bin/sh
 
 DEVICE_FILE="/dev/smd7"
-TIMEOUT=4
+TIMEOUT=4  # Set a timeout for the response
 
-# Make sure to clean up /tmp/device_output before we start
-rm -f /tmp/device_output
+# Check if the device file exists and is a character special file
+if [ ! -c "$DEVICE_FILE" ]; then
+    echo "Error: The device $DEVICE_FILE does not exist or is not a character special file."
+    exit 1
+fi
 
-# Function to send AT command to the device and read the response
-send_at_command() {
-    # Flush any existing content in the device buffer
-    cat "$DEVICE_FILE" > /dev/null 2>&1 &
+# Function to configure device and read output
+setup_device_and_read() {
+    # Start cat in the background to read from device
+    cat "$DEVICE_FILE" > /tmp/device_readout &
+    # Record cat's process ID
     CAT_PID=$!
-    sleep 0.1
-    kill -9 $CAT_PID
-    wait $CAT_PID 2>/dev/null
+    echo "Listening on $DEVICE_FILE with PID $CAT_PID"
+}
 
+# Function to send AT command to the device
+send_at_command() {
     # Prompt user for the AT command
     echo "Enter AT command: "
     read at_command
-
     # Send the AT command to the device
     echo -e "${at_command}\r" > "$DEVICE_FILE"
-
-    # Wait a moment to let the device start responding
-    sleep 0.5
-
-    # Use 'timeout' to read the response for a specified time and output to a file
-    timeout $TIMEOUT cat "$DEVICE_FILE" > /tmp/device_output &
-
-    TIMEOUT_PID=$!
-    echo "Waiting for response (PID: $TIMEOUT_PID)..."
-
-    # Wait for timeout to finish
-    wait $TIMEOUT_PID
-
-    # Check if timeout process exited normally or was killed
-    if [ $? -eq 0 ]; then
-        echo "Response received within the timeout period:"
-        cat /tmp/device_output
-    else
-        echo "Error: No response received or response timed out."
-    fi
-
-    # Clean up
-    rm -f /tmp/device_output
+    echo "Command sent, waiting for response..."
 }
 
+# Function to kill the cat process after a timeout and display the output
+kill_cat_process_and_show_output() {
+    # Sleep for the specified timeout to allow a response
+    sleep "$TIMEOUT"
+    # Kill the cat process
+    kill "$CAT_PID"
+    wait "$CAT_PID" 2>/dev/null
+    echo "Stopped listening on $DEVICE_FILE. Response is:"
+    # Output the captured data
+    cat /tmp/device_readout
+    # Clean up
+    rm /tmp/device_readout
+}
+
+# Start the script execution
+setup_device_and_read
 send_at_command
+kill_cat_process_and_show_output
