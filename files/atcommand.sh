be31eeccf2b54051d0ef81461c280aee81c3fac7
iamromulan <50184035+iamromulan@users.noreply.github.com>
2023-11-13 00:00:30 -0500

    Update atcommand.sh

--- files/atcommand.sh
+++ files/atcommand.sh
@@ -3,46 +3,65 @@
 DEVICE_FILE="/dev/smd7"
 TIMEOUT=4  # Set a timeout for the response
 
-# Check if the device file exists and is a character special file
-if [ ! -c "$DEVICE_FILE" ]; then
-    echo "Error: The device $DEVICE_FILE does not exist or is not a character special file."
-    exit 1
-fi
-
-# Function to configure device and read output
-setup_device_and_read() {
-    # Start cat in the background to read from device
+# Function to start listening to the device output in the background
+start_listening() {
+    # Start 'cat' in the background to read from device
     cat "$DEVICE_FILE" > /tmp/device_readout &
-    # Record cat's process ID
+    # Save the background process' PID
     CAT_PID=$!
-    echo "Listening on $DEVICE_FILE with PID $CAT_PID"
 }
 
-# Function to send AT command to the device
+# Function to send the AT command to the device
 send_at_command() {
     # Prompt user for the AT command
     echo "Enter AT command: "
     read at_command
     # Send the AT command to the device
     echo -e "${at_command}\r" > "$DEVICE_FILE"
-    echo "Command sent, waiting for response..."
 }
 
-# Function to kill the cat process after a timeout and display the output
-kill_cat_process_and_show_output() {
-    # Sleep for the specified timeout to allow a response
-    sleep "$TIMEOUT"
-    # Kill the cat process
+# Function to monitor the device output for a response
+wait_for_response() {
+    local start_time=$(date +%s)
+    local current_time
+    local elapsed_time
+
+    echo "Command sent, waiting for response..."
+    # Continuously check for 'OK' or 'ERROR' in the output
+    while true; do
+        # Break the loop if 'OK' or 'ERROR' is found in the output file
+        if grep -qe "OK" -e "ERROR" /tmp/device_readout; then
+            echo "Response received:"
+            cat /tmp/device_readout
+            return 0
+        fi
+
+        # Check if the timeout has been reached
+        current_time=$(date +%s)
+        elapsed_time=$((current_time - start_time))
+        if [ "$elapsed_time" -ge "$TIMEOUT" ]; then
+            echo "Error: Response timed out."
+            return 1
+        fi
+        # Check the file every second for new output
+        sleep 1
+    done
+}
+
+# Function to clean up the listening process
+cleanup() {
+    # Kill the background 'cat' process and clean up temporary files
     kill "$CAT_PID"
     wait "$CAT_PID" 2>/dev/null
-    echo "Stopped listening on $DEVICE_FILE. Response is:"
-    # Output the captured data
-    cat /tmp/device_readout
-    # Clean up
-    rm /tmp/device_readout
+    rm -f /tmp/device_readout
 }
 
-# Start the script execution
-setup_device_and_read
-send_at_command
-kill_cat_process_and_show_output
+# Main execution flow
+if [ -c "$DEVICE_FILE" ]; then
+    start_listening
+    send_at_command
+    wait_for_response
+    cleanup
+else
+    echo "Error: Device $DEVICE_FILE does not exist or is not a character special file."
+fi
