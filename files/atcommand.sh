a55e8f23739ca77994d9082e2b1978ba5c0ebd27
iamromulan <50184035+iamromulan@users.noreply.github.com>
2023-11-12 23:34:52 -0500

    Update atcommand.sh

--- files/atcommand.sh
+++ files/atcommand.sh
@@ -3,42 +3,47 @@
 DEVICE_FILE="/dev/smd7"
 TIMEOUT=4
 
-# Ensure the device file exists
-if [ ! -c "$DEVICE_FILE" ]; then
-    echo "Error: Device $DEVICE_FILE does not exist or is not a character special file."
-    exit 1
-fi
+# Make sure to clean up /tmp/device_output before we start
+rm -f /tmp/device_output
 
-# Function to send the AT command to the device
+# Function to send AT command to the device and read the response
 send_at_command() {
-    # Clear device buffer by reading any pending data
-    cat "$DEVICE_FILE" > /dev/null 2>&1 & 
+    # Flush any existing content in the device buffer
+    cat "$DEVICE_FILE" > /dev/null 2>&1 &
     CAT_PID=$!
     sleep 0.1
     kill -9 $CAT_PID
     wait $CAT_PID 2>/dev/null
 
+    # Prompt user for the AT command
+    echo "Enter AT command: "
+    read at_command
+
     # Send the AT command to the device
-    echo -e "$1\r" > "$DEVICE_FILE"
+    echo -e "${at_command}\r" > "$DEVICE_FILE"
 
-    # Read the response
-    RESPONSE=""
-    { 
-        # Give device a moment to start responding
-        sleep 0.2
-        # Capture the response for a duration of TIMEOUT
-        RESPONSE=$(timeout $TIMEOUT cat "$DEVICE_FILE")
-    }
+    # Wait a moment to let the device start responding
+    sleep 0.5
 
-    if [ -z "$RESPONSE" ]; then
-        echo "Error: Response timed out or no data received."
+    # Use 'timeout' to read the response for a specified time and output to a file
+    timeout $TIMEOUT cat "$DEVICE_FILE" > /tmp/device_output &
+
+    TIMEOUT_PID=$!
+    echo "Waiting for response (PID: $TIMEOUT_PID)..."
+
+    # Wait for timeout to finish
+    wait $TIMEOUT_PID
+
+    # Check if timeout process exited normally or was killed
+    if [ $? -eq 0 ]; then
+        echo "Response received within the timeout period:"
+        cat /tmp/device_output
     else
-        echo "Response from device:"
-        echo "$RESPONSE"
+        echo "Error: No response received or response timed out."
     fi
+
+    # Clean up
+    rm -f /tmp/device_output
 }
 
-# Main execution flow
-printf "Enter AT command: "
-read at_command
-send_at_command "$at_command"
+send_at_command
